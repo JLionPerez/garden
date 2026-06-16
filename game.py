7f9540bd3c424f993b4e5df0f3266ac1458937c4
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2026-06-16 13:05:50 -0700

    Add init of Player + random weapon selection

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import random
+
 class Enemy:
     def __init__(self, type, hp, dmg):
         self.type = type
@@ -10,11 +12,44 @@ class Player:
         self.hp = hp 
         self.item = item #changes depending on what type of weapon they have if they don't have one it's 3 dmg (barehanded)
 
+class Weapon:
+    def __init__(self, name, hp, dmg):
+        self.name = name
+        self.hp = hp
+        self.dmg = dmg
+
+ladybugs = Weapon("Ladybugs", 8, 4)
+flySwatter = Weapon("Fly Swatter", 12, 6)
+weedWhacker = Weapon("Weed Whacker", 16, 8)
+bugSpray = Weapon("Bug Spray", 20, 10)
+
+weaponCache = []
+weaponCache.append(ladybugs)
+weaponCache.append(flySwatter)
+weaponCache.append(weedWhacker)
+weaponCache.append(bugSpray)
+
+print("Welcome to the Flower Fields!")
+name = input("Please enter your name: ")
+
+item = random.choice(weaponCache)
+
+player = Player(name, 20, item)
+
+print("Hello " + player.name + "! You have an HP of " + str(player.hp) + " and your starting weapon is " + player.item.name + "." )
+
+# weaponCache = {
+#     "Ladybugs": 4,
+#     "Fly Swatter": 6,
+#     "Weed Whacker": 8,
+#     "Bug Spray": 10
+# }
+
 # goal: survive and make a bouquet of flowers from the fields you go through
 
 # player has 20 hp
 # asks for player's name will be referred to that name in the game
-# randomized weapon at start
+# randomized weapon at start, can only equip one weapon at a time
 # weapons:
     # weed whacker, dmg: 8
     # fly swatter, dmg: 6
