443c1e7ecfb7ed9462b10be2f5a39c41e22fdb02
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2026-06-16 14:16:56 -0700

    Add getters and setters to Player and Weapon classes + update spacing between text

--- game.py
+++ game.py
@@ -1,8 +1,8 @@
 import random
 
 class Enemy:
-    def __init__(self, type, hp, dmg):
-        self.type = type
+    def __init__(self, name, hp, dmg):
+        self.name = name
         self.hp = hp
         self.dmg = dmg
 
@@ -12,38 +12,45 @@ class Player:
         self.hp = hp 
         self.item = item #changes depending on what type of weapon they have if they don't have one it's 3 dmg (barehanded)
 
+    def get_name(self):
+        return self.name
+    
+    def get_hp(self):
+        return self.hp
+    
+    def get_item_name(self):
+        return self.item.get_name()
+    
+    def get_item_hp(self):
+        return self.item.get_hp()
+    
+    def get_item_dmg(self):
+        return self.item.get_dmg()
+    
+
+
 class Weapon:
     def __init__(self, name, hp, dmg):
         self.name = name
         self.hp = hp
         self.dmg = dmg
 
-ladybugs = Weapon("Ladybugs", 8, 4)
-flySwatter = Weapon("Fly Swatter", 12, 6)
-weedWhacker = Weapon("Weed Whacker", 16, 8)
-bugSpray = Weapon("Bug Spray", 20, 10)
+    def get_name(self):
+        return self.name
+    
+    def get_hp(self):
+        return self.hp
 
-weaponCache = []
-weaponCache.append(ladybugs)
-weaponCache.append(flySwatter)
-weaponCache.append(weedWhacker)
-weaponCache.append(bugSpray)
+    def set_hp(self, dmg):
+        return self.hp - dmg
+    
+    def get_dmg(self):
+        return self.dmg
 
-print("Welcome to the Flower Fields!")
-name = input("Please enter your name: ")
-
-item = random.choice(weaponCache)
-
-player = Player(name, 20, item)
-
-print("Hello " + player.name + "! You have an HP of " + str(player.hp) + " and your starting weapon is " + player.item.name + "." )
-
-# weaponCache = {
-#     "Ladybugs": 4,
-#     "Fly Swatter": 6,
-#     "Weed Whacker": 8,
-#     "Bug Spray": 10
-# }
+class Room:
+    def __init__(self, name, enemies):
+        self.name = name
+        self.enemies = enemies
 
 # goal: survive and make a bouquet of flowers from the fields you go through
 
@@ -55,9 +62,40 @@ print("Hello " + player.name + "! You have an HP of " + str(player.hp) + " and y
     # fly swatter, dmg: 6
     # bug spray, dmg: 10
     # ladybugs, dmg: 4
+
+ladybug = Weapon("Ladybug", 8, 4)
+flySwatter = Weapon("Fly Swatter", 12, 6)
+weedWhacker = Weapon("Weed Whacker", 16, 8)
+bugSpray = Weapon("Bug Spray", 20, 10)
+
+weaponCache = []
+weaponCache.append(ladybug)
+weaponCache.append(flySwatter)
+weaponCache.append(weedWhacker)
+weaponCache.append(bugSpray)
+
+print("\nWelcome to the Flower Fields!")
+name = input("Please enter your name: ")
+
+item = random.choice(weaponCache)
+
+player = Player(name, 20, item)
+
+print("\nHello " + player.name + "!\nYou have an HP of " + str(player.hp) + " and your starting weapon is " + player.get_item_name() + "." )
+print("Weapon stats of " + player.get_item_name() + ": ")
+print("HP: " + str(player.get_item_hp()) + "    Damage: " + str(player.get_item_dmg()))
+
 # 6 rooms generated: lavender field, marigold field, sunflower field, rose field, dahlia field, daisies field
     # each room have 4 entities
         # 4 enemies OR 3 enemies and 1 item
+        # to do this we have a list say 5
+        # 4 enemies and 1 weapon 
+        # randomly select a weapon add that to the list of 5
+        # randomly select enemies and add that to the list of 5 too
+        # they will be added into the list randomly so use random.shuffle()
+        # first 4 from the list will be the entities in the room, thats how we do the may or may not have weapon in room part
+
+
 # player chooses what field to start with
 # enemies:
     # weak: 
@@ -70,4 +108,3 @@ print("Hello " + player.name + "! You have an HP of " + str(player.hp) + " and y
         # Garden Snail, hp and dmg: d8
         # Earworm, hp and dmg: d10
 # clearing out a room gives a flower of from that field to the player
-
