a29112e7d1c672f4c9b81b45bf06d2b0811230e3
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2026-06-18 11:30:15 -0700

    Add enemy generation for rooms + testing enemy initi

--- game.py
+++ game.py
@@ -1,29 +1,32 @@
 import random
 
 class Enemy:
-    def __init__(self, name, hp, dmg):
+    def __init__(self, name, hp, atk):
         self.name = name
         self.hp = hp
-        self.dmg = dmg
+        self.atk = atk
 
-        def get_name(self):
-            return self.name
-        
-        def get_hp(self):
-            return self.hp
-        
-        def get_dmg(self):
-            return self.dmg
-        
-        def set_hp(self, dmg):
-            newHP = self.hp - dmg
-            self.hp = newHP
+    def get_name(self):
+        return self.name
+    
+    def get_hp(self):
+        return self.hp
+    
+    def get_atk(self):
+        return self.atk
+    
+    def set_hp(self, dmg):
+        newHP = self.hp - dmg
+        self.hp = newHP
+    
+    def get_class_name(self):
+        return self.__class__.__name__
 
 class Weapon:
-    def __init__(self, name, hp, dmg):
+    def __init__(self, name, hp, atk):
         self.name = name
         self.hp = hp
-        self.dmg = dmg
+        self.atk = atk
 
     def get_name(self):
         return self.name
@@ -35,14 +38,17 @@ class Weapon:
         newHP = self.hp - dmg
         self.hp = newHP
     
-    def get_dmg(self):
-        return self.dmg
+    def get_atk(self):
+        return self.atk
+    
+    def get_class_name(self):
+        return self.__class__.__name__
 
 class Player:
     def __init__(self, name, hp, item):
         self.name = name
         self.hp = hp 
-        self.item = item #changes depending on what type of weapon they have if they don't have one it's 3 dmg (barehanded)
+        self.item = item #barehand is 3 atk
 
     def get_name(self):
         return self.name
@@ -66,31 +72,22 @@ class Player:
     def set_item_hp(self, dmg):
         self.item.set_hp(dmg)
     
-    def get_item_dmg(self):
-        return self.item.get_dmg()
+    def get_item_atk(self):
+        return self.item.get_atk()
 
 class Room:
     def __init__(self, name, enemies):
         self.name = name
         self.enemies = enemies
 
+    def get_name(self):
+        return self.name
+
     def get_enemies(self):
         return self.enemies
     
-    def set_enemies(self, newList):
-        for e in newList:
-            self.enemies.append(e)
-
-# goal: survive and make a bouquet of flowers from the fields you go through
-
-# player has 20 hp
-# asks for player's name will be referred to that name in the game
-# randomized weapon at start, can only equip one weapon at a time
-# weapons:
-    # weed whacker, dmg: 8
-    # fly swatter, dmg: 6
-    # bug spray, dmg: 10
-    # ladybugs, dmg: 4
+    def get_enemy_name(self, i):
+        return self.enemies[i].get_name()
 
 def generateWeapons():
     ladybug = Weapon("Ladybug", 8, 4)
@@ -103,6 +100,37 @@ def generateWeapons():
     
     return item
 
+def generateEnemies():
+    aphidVals = random.randrange(1, 4)
+    spiderMiteVals = random.randrange(1, 4)
+    stinkBugVals = random.randrange(1, 6)
+    slugVals = random.randrange(1, 6)
+    gardenSnailVals = random.randrange(2, 8)
+    earwormVals = random.randrange(3, 10)
+    mouseSpiderVals = random.randrange(4, 12)
+
+    aphid = Enemy("Aphid", aphidVals, aphidVals)
+    spiderMite = Enemy("Spider Mite", spiderMiteVals, spiderMiteVals)
+    stinkBug = Enemy("Stink Bug", stinkBugVals, stinkBugVals)
+    slug = Enemy("Slug", slugVals, slugVals)
+    mouseSpider = Enemy("Mouse Spider", mouseSpiderVals, mouseSpiderVals)
+    gardenSnail = Enemy("Garden Snail", gardenSnailVals, gardenSnailVals)
+    earworm = Enemy("Earworm", earwormVals, earwormVals)
+
+    item = generateWeapons()
+
+    randomList = [aphid, spiderMite, stinkBug, slug, mouseSpider, gardenSnail, earworm, item]
+    random.shuffle(randomList)
+
+    roomList = randomList[:4]
+
+    return roomList
+
+def generateRooms():
+    pass
+
+# goal: survive and make a bouquet of flowers from the fields you go through
+# asks for player's name will be referred to that name in the game
 print("\nWelcome to the Flower Fields!")
 name = input("Please enter your name: ")
 
@@ -110,7 +138,7 @@ player = Player(name, 20, generateWeapons())
 
 print("\nHello " + player.name + "!\nYou have an HP of " + str(player.hp) + " and your starting weapon is " + player.get_item_name() + "." )
 print("Weapon stats of " + player.get_item_name() + ": ")
-print("HP: " + str(player.get_item_hp()) + "    Damage: " + str(player.get_item_dmg()))
+print("HP: " + str(player.get_item_hp()) + "    Damage: " + str(player.get_item_atk()))
 
 # testing new item pick up
 # player.set_item_hp(2)
@@ -118,7 +146,15 @@ print("HP: " + str(player.get_item_hp()) + "    Damage: " + str(player.get_item_
 
 # item = generateWeapons()
 # player.set_item(item)
+# print(player.get_item_name())
 # print(player.get_item_hp())
+
+# testing if enemies are present in room
+# firstRoom = Room("Lavender Field", generateEnemies())
+# print("\n" + firstRoom.get_name() + " has enemies of: ")
+# for e in firstRoom.enemies:
+#     print( e.get_class_name() + ": " + e.get_name() + "    HP: " + str(e.get_hp()))
+    # print("HP: " + str(e.get_hp()))
 
 # 6 rooms generated: lavender field, marigold field, sunflower field, rose field, dahlia field, daisies field
     # each room have 4 entities
@@ -132,18 +168,5 @@ print("HP: " + str(player.get_item_hp()) + "    Damage: " + str(player.get_item_
 
 
 # player chooses what field to start with
-# enemies:
-    # weak: 
-        # aphid, hp and dmg: d4
-        # spider mite, hp and dmg: d4
-        # stink bug, hp and dmg: d6
-        # slug, hp and dmg: d6
-    # strong:
-        # Mouse Spider, hp and dmg: d12
-        # Garden Snail, hp and dmg: d8
-        # Earworm, hp and dmg: d10
-def generateEnemies():
-    
-    # generate enemies then make copies of them for the rooms
 
 # clearing out a room gives a flower of from that field to the player
